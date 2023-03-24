60a424aabed22456a916d91fdc3efe2c0f3ac1c8
Edson <edson12.j@hotmail.com>
2023-03-24 19:51:41 -0300

    Exclusao de usuario

--- src/main/java/br/com/nutriclinic/api/controller/UsuarioController.java
+++ src/main/java/br/com/nutriclinic/api/controller/UsuarioController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -61,5 +62,10 @@ public class UsuarioController {
 		usuario.setSenha(senhaCriptografada);
 		
 		usuarioRepository.save(usuario);
+	}
+	
+	@DeleteMapping("/{id}")
+	public void remover(@PathVariable Long id) {
+		usuarioRepository.deleteById(id);
 	}
 }
