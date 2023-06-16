c76f05fd957806fd34020b1a2a423b0d983e30c5
Edson <edson12.j@hotmail.com>
2023-06-16 03:15:50 -0300

    Retornando descricaoapresentação no endpoint de medidas

--- src/main/java/br/com/nutriclinic/api/model/MedidaModel.java
+++ src/main/java/br/com/nutriclinic/api/model/MedidaModel.java
@@ -5,10 +5,12 @@ import br.com.nutriclinic.domain.repository.entity.Medida;
 public class MedidaModel {
 	private Long id;
 	private String descricao;
+	private String descricaoApresentacao;
 
 	public MedidaModel(Medida medida) {
 		this.id = medida.getId();
 		this.descricao = medida.getDescricao();
+		this.descricaoApresentacao = medida.getDescricaoApresentacao();
 	}
 
 	public Long getId() {
@@ -17,5 +19,9 @@ public class MedidaModel {
 
 	public String getDescricao() {
 		return descricao;
+	}
+
+	public String getDescricaoApresentacao() {
+		return descricaoApresentacao;
 	}
 }
